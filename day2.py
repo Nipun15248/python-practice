d16ff46a1a50f14c3a8d0b5855a88d720e004a5e
Nipun15248 <2020pgicanipun27@poornima.org>
2024-12-28 09:35:11 +0530

    second file

---
+++ day2.py
@@ -0,0 +1,146 @@
+# 1
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+
+# 2
+# num = 16
+# factor = 2
+# while factor <= num / 2:
+#     if num % factor == 0:
+#         print(factor, end = " ")
+#     factor += 1
+# print(num, end = " ")
+
+
+# 3
+# num = 0
+# sum = 0
+# while True:
+#     num = int(input("enter the number : "))
+#     if num < 0:
+#         break
+#     sum += num
+# print("sum =", sum)
+
+
+# 4
+# num = 13
+# while num <= 300:
+#     print(num, end = " ")
+#     num += 10
+# else :
+#     print("no should be between 10 to 300")
+
+
+# 5
+# num = 1
+# while num < 11:
+#     print(num, num ** 2)
+#     num += 1
+
+
+# 6
+# num = 105
+# while num >= 7:
+#     print(num, end = " ")
+#     num -= 7
+
+
+# 7
+# num = 2
+# while num <= 20:
+#     print(num)
+#     num += 2
+
+
+# 8
+# num = 1
+# while num <= 20:
+#     print(num)
+#     num += 2
+
+
+# 9
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+
+# 10
+# num = 0
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+
+# 11
+# num = 10
+# while num <= 10 and num >= 0:
+#     print(num)
+#     num -= 1
+
+
+# 12
+# sum = 0
+# num = 10
+# while num >= 1:
+#     sum += num
+#     num -= 1
+# print(sum)
+
+
+# 13
+# sum = 0
+# num = 2
+# while num <= 20:
+#     sum += num
+#     num += 2
+# print(sum)
+
+
+# 14
+# i = 1
+# num = int(input("enter the number to generate the table : "))
+# while i <= 10:
+#     print(num, "*", i, "=", num * i)
+#     i += 1
+
+
+# 15
+# num1 = int(input("enter the first number : "))
+# num2 = int(input("enter the second number : "))
+# while num1<=num2:
+#     if num1%2==0:
+#         print(num1)
+#     num1+=1
+
+
+# 16
+# num = int(input())
+# k=0
+# if num == 0 or num == 1:
+#     print("not a prime no")
+# else:
+#     i = 2
+#     while i < num:
+#         if num % i == 0:
+#             k += 1
+#         i += 1
+# if k == 0:
+#     print('prime')
+# else:
+#     print('not prime')
+
+
+# 17
+# i = 0
+# num = int(input())
+# while i < 10:
+#     if num % 2 == 0:
+#         print(num)
+#         i += 1
+#     num += 1
